e3e882d4ce907e7ba1f8a0e37de33015d2f36cf8
suhuashan <2891013025@qq.com>
2020-01-15 07:40:47 +0800

    [init] 项目初始化业务代码目录

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { renderRoutes } from "react-router-config";
+import { BrowserRouter } from 'react-router-dom';
+import { GlobalStyle } from "./style";
+
+import store from './store/index.js';
+import routes from "./routes/index.js";
+
+import Header from './layout/header/index.js';
+
 
 function App() {
   return (
-    <div>React APP</div>
+    <Provider store={store}>
+    <BrowserRouter>
+      <GlobalStyle></GlobalStyle>
+      <Header />
+      {renderRoutes(routes)}
+    </BrowserRouter>
+  </Provider>
   );
 }
 
