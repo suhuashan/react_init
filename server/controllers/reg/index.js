af4d7c8a2993b2eccb52af5c2b087dbf9e07b9d5
suhuashan <2891013025@qq.com>
2020-01-23 07:02:25 +0800

    [feature] 开发完成注册接口

---
+++ server/controllers/reg/index.js
@@ -0,0 +1,27 @@
+const { SuccessModel, ErrorModel } = require('../../util/response');
+const model = require('../../db/model');
+const User = model.user;
+
+const reg = async (ctx) => {
+
+    let { username, password } =  ctx.request.body,
+        res = await User.findAll({
+            where: {
+                username
+            }
+        });
+    if (res.length) {
+        ctx.body = new ErrorModel('该账号已经存在');
+    } else {
+        try {
+            await User.create({username, password});
+            ctx.body = new SuccessModel('注册成功');
+        } catch (err) {
+            ctx.body = new ErrorModel('注册失败');
+        }
+    }
+}
+
+module.exports = {
+    'POST /blog/reg': reg
+}
