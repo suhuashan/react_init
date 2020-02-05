5a83a983ba84826b50148870c22b2dd6914e282f
suhuashan <2891013025@qq.com>
2020-02-05 17:18:52 +0800

    [feature] 新增获取用户标签对应的博客接口以及分类后对应的博客接口

--- server/controllers/blog/index.js
+++ server/controllers/blog/index.js
@@ -1,6 +1,23 @@
 const { SuccessModel, ErrorModel } = require('../../util/response');
 const { article: Article, user: User } = require('../../db/model');
+const { removeDuplicate } = require('../../util/tool');
+const { Op } = require('sequelize');
 
+const ATTRIBUTES = [
+    ['id', 'blogID'],
+    ['author', 'blogAuthor'],
+    ['title', 'blogTitle'],
+    ['abstract', 'blogAbstract'],
+    ['content', 'blogContent'],
+    ['createdAt', 'blogTime'],
+    ['tags', 'blogTags'],
+    ['categories', 'blogCategories'],
+    ['read', 'blogRead'],
+    ['word', 'blogWord'],
+    ['type', 'blogType']
+];
+
+//博客保存草稿或者发布博客
 const savePublishBlog = async(ctx) => {
     let {
         blogType,
@@ -26,9 +43,20 @@ const savePublishBlog = async(ctx) => {
             word: blogContent.length,
             read: 0
         });
+
+        let res = await User.findOne({
+            attributes: [
+                'tags', 'categories'
+            ],
+            where: {
+                username: ctx.session.username
+            }
+        });
+        let { tags, categories } = res.dataValues;
+
         await User.update({
-            tags: blogTags,
-            categories: blogCategories
+            tags: removeDuplicate(tags, blogTags),
+            categories: removeDuplicate(categories, blogCategories)
         }, {
             where: {
                 username: ctx.session.username
@@ -38,29 +66,18 @@ const savePublishBlog = async(ctx) => {
         let message = blogType === 'draft' ? '保存成功' : '发布成功';
         ctx.body = new SuccessModel(message);
     } catch (err) {
-        console.log(err);
         ctx.body = new ErrorModel('操作失败');
     }
 
 };
 
+//获取首页博客列表
 const getBlogList = async (ctx) => {
     let { limit, offset } =  ctx.request.body;
     try {
         let blogListTotal = await Article.findAll({where:{status: 'published'}});
         let blogList = await Article.findAll({
-            attributes: [
-                ['id', 'blogID'],
-                ['title', 'blogTitle'],
-                ['abstract', 'blogAbstract'],
-                ['content', 'blogContent'],
-                ['createdAt', 'blogTime'],
-                ['tags', 'blogTags'],
-                ['categories', 'blogCategories'],
-                ['read', 'blogRead'],
-                ['word', 'blogWord'],
-                ['type', 'blogType']
-            ],
+            attributes: ATTRIBUTES,
             where: {
                 status: 'published'
             },
@@ -74,13 +91,59 @@ const getBlogList = async (ctx) => {
             total: blogListTotal.length
         });
     } catch (err) {
-        console.log(err);
         ctx.body = new ErrorModel('获取失败');
     }
+};
 
+//根据分类名筛选出对用的博客列表
+const getBlogListByCategories = async (ctx) => {
+    let { categoryName } = ctx.request.body;
+    try {
+        let res = await Article.findAll({
+            attributes: ATTRIBUTES,
+            where: {
+                userID: ctx.session.userID,
+                status: 'published',
+                categories: {
+                    [Op.like]:'%' +categoryName + '%'
+                }
+            }
+        });
+        ctx.body = new SuccessModel({
+            list: res,
+            total: res.length
+        });
+    } catch (e) {
+        ctx.body = new ErrorModel('获取失败');
+    }
+};
+
+//根据标签搜索出对应的博客列表
+const getBlogListByTags = async (ctx) => {
+    let { tagName } = ctx.request.body;
+    try {
+        let res = await Article.findAll({
+            attributes: ATTRIBUTES,
+            where: {
+                userID: ctx.session.userID,
+                status: 'published',
+                tags: {
+                    [Op.like]:'%' +tagName + '%'
+                }
+            }
+        });
+        ctx.body = new SuccessModel({
+            list: res,
+            total: res.length
+        });
+    } catch (e) {
+        ctx.body = new ErrorModel('获取失败');
+    }
 };
 
 module.exports = {
     'POST /blog/save_publish_blog': savePublishBlog,
-    'POST /blog/get_blog_list': getBlogList
+    'POST /blog/get_blog_list': getBlogList,
+    'POST /blog/get_blog_list/by_categories': getBlogListByCategories,
+    'POST /blog/get_blog_list/by_tags': getBlogListByTags
 }
---
+++ server/util/tool.js
@@ -0,0 +1,13 @@
+/**
+ * 除去两个字符串中的重复的字符
+ * @param {String} str1 
+ * @param {String} str2 
+ * @return {String} 
+ */
+function removeDuplicate (str1 = '', str2 = '') {
+    return [...new Set([...str1.split(','), ...str2.split(',')])].join();
+}
+
+module.exports = {
+    removeDuplicate
+};
